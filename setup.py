5f35eb7b39b06539e7039c2495bc478b4d21dc42
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2021-11-04 17:26:35 -0700

    0.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="Festerize",
-    version="0.4.1",
+    version="0.4.2",
     py_modules=["festerize"],
     install_requires=["beautifulsoup4", "click", "requests"],
     entry_points="""
